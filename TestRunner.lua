37fe29b692dc51e4f5a4c3b6f3a0fdcd9de30cb8
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-01-25 11:33:05 -0500

    Update TestRunner.lua

--- TestRunner.lua
+++ TestRunner.lua
@@ -1,3 +1,4 @@
+-- Used to run the module
 -- Will run on both client and server just fine
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local TrafficController = require( ReplicatedStorage:WaitForChild("TrafficController") );
